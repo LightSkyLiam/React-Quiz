5461a05819977aa68ea4de5eb63e64bfe21e35ae
LightSkyLiam <liamlasry2000@gmail.com>
2024-06-14 18:29:42 +0300

    added globalStyled

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { GlobalStyles } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import StartingPage from "./Starting Page/StartingPage";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./General Components/AppLayout";
+import StartingPage from "./Starting Page/StartingPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -11,14 +13,26 @@ const queryClient = new QueryClient({
   },
 });
 
+const globalStyles = (
+  <GlobalStyles
+    styles={{
+      html: { height: "100%", fontSize: "62.5%" },
+      body: { margin: 0, minHeight: "100vh" },
+    }}
+  />
+);
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      {globalStyles}
       <ReactQueryDevtools initialIsOpen={false} />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="start-quiz" />} />
-          <Route path="start-quiz" element={<StartingPage />} />
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="start-quiz" />} />
+            <Route path="start-quiz" element={<StartingPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
